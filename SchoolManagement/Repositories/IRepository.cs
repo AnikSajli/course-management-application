60edf643a5ae0a1cea1f6883458dfea9885ffb04
Md. Abul Hasan Sajli <aniksajli@gmail.com>
2021-08-23 23:14:33 +0600

    Initiated project and created student controller

---
+++ SchoolManagement/Repositories/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<Teacher>> GetTeachers();
+        Task<Teacher> GetTeacherById(string id);
+        Task<IEnumerable<Teacher>> GetTeacherByName(string name);
+        Task<IEnumerable<Student>> GetStudents();
+        Task<Student> GetStudentById(string id);
+        Task<IEnumerable<Student>> GetStudentByName(string name);
+        Task<IEnumerable<Course>> GetCourses();
+        Task<IEnumerable<Course>> GetCourseByName(string name);
+        Task<Course> GetCourseById(string id);
+        Task<CourseRegistration> GetCourseRegistrationByIdAndStudentId(string courseId, string studentId);
+        Task<IEnumerable<CourseRegistration>> GetStudentsByCourse(string courseId);
+    }
+}
