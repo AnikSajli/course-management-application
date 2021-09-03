ce3075a6c9789ffe98597fb057bb71c6d61b9ec6
Md. Abul Hasan Sajli <aniksajli@gmail.com>
2021-09-03 20:14:18 +0600

    added course model and controller

---
+++ SchoolManagement/Model/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Course
+    {
+        public string Id { get; set; }
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+        public double CourseCredit { get; set; }
+        public string TeacherId { get; set; }
+    }
+}
